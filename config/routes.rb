9ee41b59dfe4464fd8d8f82fa88d430649d2f699
Ben Schembri <benschembri@gmail.com>
2025-04-15 21:00:06 +1000

    started navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,27 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Application routes
-<<<<<<< Updated upstream
-  resources :houses, only: [:index, :create, :update, :destroy] do
-    resources :rooms, only: [:create]
-  end
-
-  resources :rooms, only: [:index, :update, :destroy] do
-    resources :boxes, only: [:create]
-  end
-
-  resources :boxes, only: [:index, :create, :destroy]
-
-  resources :items, only: [:index, :create, :update, :destroy] do
-    resources :tags, only: [:create, :destroy]
-  end
-=======
   resources :households, only: [:index, :create, :update, :destroy]
   resources :rooms, only: [:index, :create, :update, :destroy]
   resources :boxes, only: [:index, :create, :update, :destroy]
   resources :tags, only: [:index, :create, :update, :destroy]
   resources :items, only: [:index, :create, :update, :destroy]
->>>>>>> Stashed changes
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
