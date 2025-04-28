87a65d71910004a6222fd533aa1ef979d04900aa
Ben Schembri <benschembri@gmail.com>
2025-04-28 15:54:26 +1000

    upfate dependent destroy

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,6 @@
 class House < ApplicationRecord
   has_and_belongs_to_many :users
-  has_many :rooms, dependent: :destroy
+  has_many :rooms, dependent: :restrict_with_error
   has_many :boxes, through: :rooms
   has_many :items
 
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :house
-  has_many :boxes
+  has_many :boxes, dependent: :destroy
   has_many :direct_items, class_name: "Item"
   has_many :items, through: :boxes
 
