f93b467dbd297513af9e71ed4681e886aff5c755
Ben Schembri <benschembri@gmail.com>
2025-07-02 20:35:56 +1000

    feat: better login ui, flash messages auto close, ignore .yarn/

--- app/frontend/javascript/controllers/flash_controller.js
+++ app/frontend/javascript/controllers/flash_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      this.element.remove();
+    }, "4000");
+  }
+
   close() {
     this.element.remove();
   }
