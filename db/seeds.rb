f914f3dbe826e3aec544b6ba831c7adfb10f671a
Ben Schembri <benschembri@gmail.com>
2025-08-24 01:05:14 +1000

    seeds: fix tag seeds and seed second user

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,90 +4,97 @@
 #
 #
 #
-puts "Destroying Items..."
-Item.destroy_all
+ActiveRecord::Base.transaction do
+  puts "Destroying Items..."
+  Item.destroy_all
 
-puts "Destroying Boxes..."
-Box.destroy_all
+  puts "Destroying Boxes..."
+  Box.destroy_all
 
-puts "Destroying Rooms..."
-Room.destroy_all
+  puts "Destroying Rooms..."
+  Room.destroy_all
 
-puts "Destroying Houses..."
-House.destroy_all
+  puts "Destroying Houses..."
+  House.destroy_all
 
-puts "Destroying tags..."
-Tag.destroy_all
+  puts "Destroying tags..."
+  Tag.destroy_all
 
-puts "Destroying users..."
-User.destroy_all
+  puts "Destroying users..."
+  User.destroy_all
 
-puts "Creating users..."
-user = User.create!(
-  email: 'benschembri@gmail.com',
-  password: 'strongPassword~123',
-  password_confirmation: 'strongPassword~123',
-  name: 'Ben',
-)
-puts "User created: #{user.email}"
+  puts "Creating users..."
+  password = 'strongPassword~123'
 
-puts "Creating a House..."
-house = House.create(name: "Home")
-user.houses << house
-puts "House created: #{house.name}"
+  user = User.create!(
+    email: 'benschembri@gmail.com',
+    password: password,
+    password_confirmation: password,
+    name: 'Ben',
+  )
+  puts "User created: #{user.name}"
+  puts "username: #{user.email}"
+  puts "password: #{password}"
 
-puts "Creating a second House..."
-house2 = House.create(name: "Mum and dad's place")
-user.houses << house2
-puts "House created: #{house2.name}"
+  second_user = User.create!(
+    email: 'sarrahmorrmorr@gmail.com',
+    password: password,
+    password_confirmation: password,
+    name: 'Morrie',
+  )
+  puts "User created: #{second_user.name}"
+  puts "username: #{second_user.email}"
+  puts "password: #{password}"
 
-puts "Creating Rooms..."
-room = Room.create(name: 'Garage', house: house)
-puts "Room created: #{room.name}"
-5.times do
-  room = Room.create(name: Faker::House.room, house: house)
+  puts "Creating a House..."
+  house = House.create!(name: "Home")
+  user.houses << house
+  puts "House created: #{house.name}"
+
+  puts "Creating a second House..."
+  house2 = House.create!(name: "Mum and dad's place")
+  user.houses << house2
+  puts "House created: #{house2.name}"
+
+  puts "Creating Rooms..."
+  room = Room.create!(name: 'Garage', house: house)
   puts "Room created: #{room.name}"
-end
-
-puts "Creating Tags..."
-10.times do
-  tag = Tag.create(user: user, name: Faker::Hobby.activity.downcase)
-  puts "Tag created: #{tag.name}"
-end
-
-puts "Creating Boxes..."
-15.times do
-  room = house.rooms.sample
-  box = Box.create(room: room, house: room.house)
-  puts "Box created: #{box.number}"
-end
-
-puts "Creating Items..."
-100.times do
-  has_notes = [true, true, false].sample
-  if has_notes
-    item = Item.create(
-      name: Faker::Appliance.equipment,
-      notes: Faker::TvShows::HowIMetYourMother.quote,
-      box: house.boxes.sample,
-      house: house,
-      user: user
-    )
-  else
-    item = Item.create(
-      name: Faker::Appliance.equipment,
-      box: house.boxes.sample,
-      house: house,
-      user: user
-    )
+  5.times do
+    room = Room.create!(name: Faker::House.room.capitalize, house: house)
+    puts "Room created: #{room.name}"
   end
-  puts "Item created: #{item.name}"
 
-  (0..4).to_a.sample.times do
-    random_tag = Tag.where(user: user).sample
-    unless item.tags.include?(random_tag)
-      item.tags << random_tag
-      puts "Tag added: #{random_tag.name}"
+  puts "Creating Boxes..."
+  15.times do
+    room = house.rooms.sample
+    box = Box.create!(room: room, house: room.house)
+    puts "Box created: #{box.number}"
+  end
+
+  puts "Creating Tags..."
+  until Tag.count == 10 do
+    tag = Tag.create!(name: Faker::Hobby.activity.downcase) rescue ActiveRecord::RecordInvalid
+    puts "Tag created: #{tag.name}"
+  end
+
+  puts "Creating Items..."
+  tags = Tag.all
+  100.times do
+    has_notes = [true, true, false].sample
+    box = house.boxes.sample
+    item = Item.create!(
+      name: Faker::Appliance.equipment,
+      notes: has_notes ? Faker::TvShows::HowIMetYourMother.quote[0, 255] : nil,
+      box: box,
+      house: house,
+      room: box.room,
+      user: user
+    )
+    puts "Item created: #{item.name}"
+
+    (0..4).to_a.sample.times do
+      random_tagging = Tagging.find_or_create_by!(item: item, tag: tags.sample)
+      puts "Tag '#{random_tagging.tag.name}' added to item '#{item.name}'"
     end
   end
 end
