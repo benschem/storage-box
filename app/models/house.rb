1e25f1a385c9c331ad9ffa6b0965a56c406e16b0
Ben Schembri <benschembri@gmail.com>
2025-09-11 20:03:33 +1000

    feat: add dependent restrict with errors to House.items

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
+# A house with rooms where items are stored
 class House < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :rooms, dependent: :restrict_with_error
   has_many :boxes, through: :rooms
-  has_many :items
+  has_many :items, dependent: :restrict_with_error
   has_many :invites, dependent: :destroy
 
   validates :name, presence: true
