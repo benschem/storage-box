074f94239e4ae58c5cb1d5880b377353f4c7591b
Ben Schembri <benschembri@gmail.com>
2026-01-18 19:02:56 +1100

    refactor: remove user has many rooms and boxes - not using them

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_and_belongs_to_many :houses # optional: true is the default for has_and_belongs_to_many
-  has_many :rooms, through: :houses # TODO: Does it need this?
-  has_many :boxes, through: :rooms # TODO: Does it need this?
 
   # TODO: come back to this and add options for the user
   # to transfer their items to a house or fully delete them.
