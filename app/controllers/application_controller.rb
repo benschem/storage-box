12f702fb04e7dc0545de285e2f7e8d8b8557aaf7
Ben Schembri <benschembri@gmail.com>
2026-01-18 18:59:27 +1100

    refactor: make a method private and lint application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Base controller
 class ApplicationController < ActionController::Base
-  include ActionView::RecordIdentifier  # Needed to use dom_id
+  include ActionView::RecordIdentifier # Needed to use dom_id
   include Pagy::Backend
   include Pundit::Authorization
 
@@ -8,31 +11,28 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :house_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name house_id])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :house_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name house_id])
   end
 
   after_action :verify_pundit_authorization, unless: :skip_pundit?
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_back_or_to(root_path)
-  end
 
   private
 
   def verify_pundit_authorization
-    if action_name == "index"
-      verify_policy_scoped
-    else
-      verify_authorized
-    end
+    action_name == 'index' ? verify_policy_scoped : verify_authorized
   end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.' # rubocop:disable Rails/I18nLocaleTexts
+    redirect_back_or_to(root_path)
   end
 end
